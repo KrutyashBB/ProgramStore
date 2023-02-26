d328ac0002a2a118b8c0037459763f5f05cdd803
Legenda <kostrov2525@gmail.com>
2023-02-26 22:32:28 +0500

    guarantess, started making reviews

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
 from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.widgets import TextArea
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import date
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+class Reviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(70), nullable=False)
+    review = db.Column(db.Text, nullable=False)
+    date_added = db.Column(db.DateTime, default=date.today())
+
+
+class ReviewForm(FlaskForm):
+    username = StringField('Имя', validators=[DataRequired()])
+    review = StringField('Отзыв', validators=[DataRequired()], widget=TextArea())
+    submit = SubmitField('Отправить')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
+
+
+@app.route('/guarantees')
+def guarantees():
+    return render_template('guarantees.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
