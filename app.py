e8fe40eeb6c193f403259f155c1da1700c1125af
Legenda <kostrov2525@gmail.com>
2023-03-03 20:05:01 +0500

    form registration, form add_product

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, session, render_template, url_for, flash, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import StringField, SubmitField, EmailField, IntegerField, PasswordField, validators
 from wtforms.widgets import TextArea
 from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import date
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -21,10 +23,43 @@ class Reviews(db.Model):
     date_added = db.Column(db.Date, default=date.today())
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+
 class ReviewForm(FlaskForm):
     username = StringField('Имя', validators=[DataRequired()])
     review = StringField('Отзыв', validators=[DataRequired()], widget=TextArea())
     submit = SubmitField('Отправить')
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Имя", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password_hash = PasswordField('Пароль', [validators.DataRequired(),
+                                             validators.EqualTo('password_hash2', message='Пароли должны совпадать')])
+    password_hash2 = PasswordField('Подтвердите Пароль')
+    submit = SubmitField("Зарегистрироваться")
+
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired()])
+    password_hash = PasswordField('Пароль', [validators.DataRequired()])
+    submit = SubmitField("Войти")
+
+
+class AddProductForm(FlaskForm):
+    name = StringField("Название", validators=[DataRequired()])
+    price = IntegerField("Цена", validators=[DataRequired()])
+    stock = IntegerField("Количество", validators=[DataRequired()])
+    description = StringField('Описание', validators=[DataRequired()], widget=TextArea())
+    img_1 = FileField('Фото 1', validators=[FileRequired(), FileAllowed(['jpg', 'png'])])
+    img_2 = FileField('Фото 2', validators=[FileRequired(), FileAllowed(['jpg', 'png'])])
+    img_3 = FileField('Фото 3', validators=[FileRequired(), FileAllowed(['jpg', 'png'])])
+    submit = SubmitField("Добавить")
 
 
 @app.route('/reviews', methods=['GET', 'POST'])
@@ -38,6 +73,42 @@ def reviews():
         form.username.data = ''
         form.review.data = ''
     return render_template('reviews.html', form=form, reviews=reviews)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        hashed_pw = generate_password_hash(form.password_hash.data, 'sha256')
+        user = User(name=form.name.data, email=form.email.data, password_hash=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались')
+        return redirect(url_for('index'))
+    our_users = User.query.order_by(User.name)
+    return render_template('register.html', form=form, our_users=our_users)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if check_password_hash(user.password_hash, form.password_hash.data):
+                flash('Вы успешно вошли!')
+                return redirect(url_for('index'))
+            else:
+                flash('Неправильный пароль')
+        else:
+            flash('Такого пользователя не существует')
+    return render_template('login.html', form=form)
+
+
+@app.route('/add-product', methods=['GET', 'POST'])
+def add_product():
+    form = AddProductForm()
+    return render_template('add-product.html', form=form)
 
 
 @app.route('/')
